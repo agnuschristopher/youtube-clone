bb40b5fded54f292ac40c61507df5dd77d4e6fb8
agnuschristopher <agnuschristo0706@gmail.com>
2026-06-26 16:57:23 +0530

    Enhance vite.config.js with base path comment
    Added comment to explain the base path in Vite config.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/youtube-clone/', // 👈 Add this line (include the forward slashes!)
+  base: '/youtube-clone/', // 👈 This tells Vite where to link your main index.js and styling assets!
 })
