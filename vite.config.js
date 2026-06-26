69b256db010540eb88ef68657fc3bd976c0f033a
agnuschristopher <agnuschristo0706@gmail.com>
2026-06-26 17:45:03 +0530

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/youtube-clone/', // 👈 This tells Vite where to link your main index.js and styling assets!
+  base: '/youtube-clone/', // 👈 Make sure the forward slashes are exactly like this!
 })
